46514fde9196c9494924f122155660288744f3d3
Remy Guts <remyguts@gmail.com>
2020-03-25 14:49:23 -0700

    connect mongo

--- server.js
+++ server.js
@@ -9,3 +9,17 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 const path = require(`path`);
 const db = require(`./models`);
+
+app.use(logger(`dev`));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(`public`));
+
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    `mongodb://<dbuser>:<dbpassword>@ds335957.mlab.com:35957/heroku_lb6j4qxr`,
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false
+  }
+);
