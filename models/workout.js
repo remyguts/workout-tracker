63a5a730cd981ea4d931b72401dbf809f9c34c37
Remy Guts <remyguts@gmail.com>
2020-03-25 14:59:07 -0700

    write schema

--- models/workout.js
+++ models/workout.js
@@ -3,3 +3,46 @@
 const mongoose = require(`mongoose`);
 
 const Schema = mongoose.Schema;
+const workoutSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: Date.now
+    },
+    exercises: [
+      {
+        type: {
+          type: String,
+          trim: true,
+          required: `Enter a type of exercise`
+        },
+        name: {
+          type: String,
+          trim: true,
+          required: `Enter a name for the exercise`
+        },
+        duration: {
+          type: Number,
+          required: `Enter a duration`
+        },
+        weight: {
+          type: Number
+        },
+        reps: {
+          type: Number
+        },
+        sets: {
+          type: Number
+        },
+        distance: {
+          type: Number
+        }
+      }
+    ]
+  },
+  {
+    toJSON: {
+      virtuals: true
+    }
+  }
+);
