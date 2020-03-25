9d4ba869b61f74520a96993d7dfb48f3191feb6b
Remy Guts <remyguts@gmail.com>
2020-03-25 14:46:56 -0700

    variables for serverjs

---
+++ server.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const express = require(`express`);
+const logger = require(`morgan`);
+const mongoose = require(`mongoose`);
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+const path = require(`path`);
+const db = require(`./models`);
