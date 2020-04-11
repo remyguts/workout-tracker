a292c9d935667bc0aafa6cbfc4fcd8d097969f22
Remy Guts <remyguts@gmail.com>
2020-04-11 14:23:09 -0700

    fixing heroku db problems

--- server.js
+++ server.js
@@ -17,64 +17,64 @@ app.use(express.static(`public`));
 
 mongoose.connect(
   process.env.MONGODB_URI ||
-    `mongodb://<dbuser>:<dbpassword>@ds335957.mlab.com:35957/heroku_lb6j4qxr`,
+    `mongodb://user:password1@ds131139.mlab.com:31139/heroku_9jgfmkc6`,
   {
     useNewUrlParser: true,
-    useFindAndModify: false
+    useFindAndModify: false,
   }
 );
 app.listen(PORT, () => {
   console.log(`App running on http://localhost:${PORT}`);
 });
 
-app.get(`/exercise`, function(req, res) {
+app.get(`/exercise`, function (req, res) {
   res.sendFile(path.join(__dirname, `./public/exercise.html`));
 });
 
-app.get(`/`, function(req, res) {
+app.get(`/`, function (req, res) {
   res.sendFile(path.join(__dirname, `./public/index.html`));
 });
 
-app.get(`/stats`, function(req, res) {
+app.get(`/stats`, function (req, res) {
   res.sendFile(path.join(__dirname, `./public/stats.html`));
 });
 
 app.get(`/api/workouts`, (req, res) => {
   db.Workout.find({})
-    .then(dbWorkout => {
+    .then((dbWorkout) => {
       res.json(dbWorkout);
     })
-    .catch(err => {
+    .catch((err) => {
       res.json(err);
     });
 });
 
 app.get(`/api/workouts/range`, (req, res) => {
   db.Workout.find({})
-    .then(dbWorkout => {
+    .then((dbWorkout) => {
       res.json(dbWorkout);
     })
-    .catch(err => {
+    .catch((err) => {
       res.json(err);
     });
 });
 
 app.post(`/api/workouts`, (req, res) => {
   db.Workout.create({})
-    .then(newWorkout => {
+    .then((newWorkout) => {
       res.json(newWorkout);
     })
-    .catch(err => {
+    .catch((err) => {
       res.json(err);
     });
 });
 
 app.put(`/api/workouts/:id`, (req, res) => {
   db.Workout.update({ _id: req.params.id }, { $push: { exercises: req.body } })
-    .then(updatedWorkout => {
+    .then((updatedWorkout) => {
       res.json(updatedWorkout);
     })
-    .catch(err => {
+    .catch((err) => {
       res.json(err);
     });
 });
